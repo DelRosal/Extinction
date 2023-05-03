d22933b510b0999c53e119dbfd075ddf579d916e
Iker Ochoa <79234651+Iker8av@users.noreply.github.com>
2023-05-02 20:10:00 -0600

    fixed

--- Assets/Scripts/Damage.cs
+++ Assets/Scripts/Damage.cs
@@ -8,19 +8,35 @@ public class Damage : MonoBehaviour
     [SerializeField]
     private Animator anim;
 
+    [SerializeField]
+    private AudioSource collisonSound;
+
+
+    //We add the health bar
+    public HealthBar healthBar;
+
+    public int maxHealth = 50;
+    public int currentHealth;
+
+    //We add sound for the death
+    [SerializeField]
+    private AudioSource deathSound;
+
     public float repelBulletForce = 100f;
     public float repelEnemyForce = 300f;
 
     private Rigidbody2D rb;
     private Movimiento_new movimiento;
 
-    void Start(){
+    void Start()
+    {
         rb = GetComponent<Rigidbody2D>();
         movimiento = GetComponent<Movimiento_new>();
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.CompareTag("Bullet")){
+        if (collision.CompareTag("Bullet"))
+        {
             Debug.Log("Damage");
             anim.SetTrigger("Damage");
 
@@ -29,7 +45,8 @@ public class Damage : MonoBehaviour
             movimiento.movVel.x = 0;
             rb.AddForce(repelForceVec, ForceMode2D.Impulse);
         }
-        else if (collision.CompareTag("Patrol Enemy")){
+        else if (collision.CompareTag("Patrol Enemy"))
+        {
             Debug.Log("Damage");
             anim.SetTrigger("Damage");
 
@@ -38,14 +55,15 @@ public class Damage : MonoBehaviour
             movimiento.movVel.x = 0;
             rb.AddForce(repelForceVec, ForceMode2D.Impulse);
         }
-        else if (collision.CompareTag("Enemy")){
+        else if (collision.CompareTag("Enemy"))
+        {
             Debug.Log("Damage");
             anim.SetTrigger("Damage");
             collisonSound.Play();
             //We call the function to take damage
             TakeDamage(10);
-            
-        } 
+
+        }
     }
 
     private void OnCollisionEnter2D(Collision2D other)
@@ -59,8 +77,31 @@ public class Damage : MonoBehaviour
             TakeDamage(10);
         }
 
-        
+
     }
 
-    
+    void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+        healthBar.SetHealth(currentHealth);
+        healthBar.SetHealth(currentHealth);
+        if (currentHealth <= 0)
+        {
+            //We only want to play the animation of death once
+            anim.SetTrigger("Death");
+            Debug.Log("Death");
+            //We play the sound of death
+            deathSound.Play();
+
+            StartCoroutine(DeathFunction());
+
+        }
+
+    }
+
+    IEnumerator DeathFunction()
+    {
+        yield return new WaitForSeconds(1f);
+        SceneManager.LoadScene("Game Over");
+    }
 }
